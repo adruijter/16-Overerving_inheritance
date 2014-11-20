dff13ad13d0267bb5d6b94651b9a6901c01c183c
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-20 09:13:27 +0100

    Opdracht uitgebreid

--- Overerving_inheritance/Camper.cs
+++ Overerving_inheritance/Camper.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Overerving_inheritance
 {
-    public class Camper
+    public class Camper : Voertuig
     {
         /* Fields
          * aantalPersonen
@@ -14,6 +14,20 @@ namespace Overerving_inheritance
          * leefOppervlak
          * aantalBedden
          * 
+         * Laat deze class erven van voertuig
+         * 
+         * Maak de properties van deze velden
+         * 
+         * Maak een constructor
+         * 
+         * Maak een Draw method die de waarde van de bovenstaande velden en de geerfde velden
+         * weergeeft
+         * 
+         * Maak tevens een object van deze class aan in de Program Class
+         */
+        public void Draw()
+        {
+        }
 
     }
 }
