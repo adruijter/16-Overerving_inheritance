7f3a45c8b3beae9e1a42fa13196e0afcf485f91d
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-20 09:09:14 +0100

    Camper Class

---
+++ Overerving_inheritance/Camper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Overerving_inheritance
+{
+    public class Camper
+    {
+        /* Fields
+         * aantalPersonen
+         * Douche
+         * Toilet
+         * leefOppervlak
+         * aantalBedden
+         * 
+
+    }
+}
